d2d5b9def7daddda3b0f6f927e4a2dd8e2d3e536
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-17 17:49:29 -0500

    Code is way too complicated so I am saving this version if mess up the restructuring

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,35 +1,42 @@
-const notes = require('express').Router();
+const router = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const { readFromFile, readAndAppend } = require('../helpers/fsUtils');
 
-
 // GET Route for retrieving all notes
-notes.get('/', (req, res) => {
+router.get('/', (req, res) => {
     readFromFile('./db/notes.json').then((data) => res.json(JSON.parse(data)));
   });
 
+// GET Route for a specific note
+router.get('/:id', (req, res) => {
+    const noteId = req.params.id;
+    readFromFile('./db/notes.json')
+      .then((data) => JSON.parse(data))
+      .then((json) => {
+        const result = json.filter((note) => note.id === noteId);
+        return result.length > 0
+          ? res.json(result)
+          : res.json('No note with that ID');
+      });
+  });
+
+
 // POST request to add a note
-notes.post('/', (req, res) => {
-    // POST request was received
+router.post('/', (req, res) => {
     console.info(`${req.method} request received to add a note`);
-  
-    // Destructuring assignment for the items in req.body
     const { title, text } = req.body;
   
-    // If all the required properties are present
     if (title && text) {
-      // Variable for the object we will save
       const newNote = {
-        title,
-        text
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
       };
-  
       readAndAppend(newNote, './db/notes.json');
-  
       const response = {
         status: 'success',
         body: newNote,
       };
-  
       console.log(response);
       res.status(201).json(response);
     } else {
@@ -37,4 +44,22 @@ notes.post('/', (req, res) => {
     }
   });
 
-  module.exports = notes;
+
+//  DELETE route for specific note
+  router.delete(`/:id`, (req, res) => {
+    const noteId = req.params.id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/notes.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Note ${id} has been deleted 🗑️`);
+    });
+});
+
+  module.exports = router;
