738aa582b8bf5edaf24cd82e0fd7a3eb7f2c34c8
Toliver Crisp <92277632+tolivercrisp@users.noreply.github.com>
2022-12-17 17:53:32 -0500

    Restructuring finished, deleted unneccesary overcomplication of routes folder, and transfered code to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -13,19 +13,69 @@ const path = require('path');
 // Middleware for parsing JSON and urlencoded form data
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use('/api', api);
 app.use(express.static('public'));
+let notes = require('./db/notes.json')
 
-// GET Route for homepage
+// GET route for homepage
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
 );
 
-// GET Route for notes page
+// GET route for notes page
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
+// GET route for notes API
+app.get("/api/notes", function (req, res) {
+  fs.readFile("./db/notes.json", "utf8", function (err, data) {
+    if (err) {
+      console.log(err);
+      return;
+    }
+    res.json(notes);
+  });
+});
+
+// POST request to create a new note
+router.post('/', (req, res) => {
+    console.info(`${req.method} request received to add a note`);
+    const { title, text } = req.body;
+  
+    if (title && text) {
+      const newNote = {
+        title: req.body.title,
+        text: req.body.text,
+        id: uuidv4(),
+      };
+      readAndAppend(newNote, './db/notes.json');
+      const response = {
+        status: 'success',
+        body: newNote,
+      };
+      console.log(response);
+      res.status(201).json(response);
+    } else {
+      res.status(500).json('Error in posting note');
+    }
+  });
+
+//  DELETE route for specific note
+  router.delete(`/:id`, (req, res) => {
+    const noteId = req.params.id;
+  readFromFile('./db/notes.json')
+    .then((data) => JSON.parse(data))
+    .then((json) => {
+      // Make a new array of all tips except the one with the ID provided in the URL
+      const result = json.filter((note) => note.id !== noteId);
+
+      // Save that array to the filesystem
+      writeToFile('./db/notes.json', result);
+
+      // Respond to the DELETE request
+      res.json(`Note ${id} has been deleted 🗑️`);
+    });
+});
 
 // Server is listening
 app.listen(PORT, () => {
